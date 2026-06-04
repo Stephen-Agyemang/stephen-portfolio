f8e553bb1f3a61085f825c6d13188fd4bcbf458a
Stephen_Agyemang <agyemangstephen2580@gmail.com>
2026-06-04 03:56:59 -0400

    refactor: migrate external font loading to local packages via fontsource

--- stephen-portfolio/src/main.jsx
+++ stephen-portfolio/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/inter'
+import '@fontsource/dm-mono/400.css'
+import '@fontsource/dm-mono/500.css'
 import './index.css'
 import App from './App.jsx'
 
